a7e90c9470448ebd05377cbec680f49c653ac363
Shakiful <shakifulkhan@gmail.com>
2023-11-13 12:17:32 +0600

    nav active status, image resized, fixed filter

--- js/script.js
+++ js/script.js
@@ -5,24 +5,48 @@ let app = angular
       // .when("/", {
       //   templateUrl: "../demo.html",
       // })
-      .when("/demo", {
+      .when("/index", {
         templateUrl: "../demo.html",
       })
-      .when("/index", {
+      .when("/demo", {
         templateUrl: "../index.html",
+        controller: "HeaderController",
       })
       .when("/directory", {
         templateUrl: "../directory.html",
         controller: "MoviesController",
       })
+      .when("/watchlist", {
+        templateUrl: "../watchlist.html",
+        controller: "MoviesController",
+      })
+
       .otherwise({
-        redirectTo: "/index",
+        redirectTo: "/index"
       });
     $locationProvider.html5Mode(true);
   })
   .controller("MoviesController", function ($scope, $http) {
+    $scope.plotLimit = 90;
+    $scope.showFullPlot = false;
     $scope.movies = [];
     $http.get("../movies.json").then(function (data) {
       $scope.movies = data.data;
     });
+
+    $scope.togglePlot = function(movie) {
+      $scope.showFullPlot = !$scope.showFullPlot;
+  
+      // Adjust the plot limit based on the toggle
+      if ($scope.showFullPlot) {
+        $scope.plotLimit = movie.Plot.length;
+      } else {
+        $scope.plotLimit = 90; // Set it to your initial limit
+      }
+    };
+  })
+  .controller("HeaderController", function ($scope, $location) {
+    $scope.isActive = function (viewLocation) {
+      return viewLocation === $location.path();
+    };
   });
